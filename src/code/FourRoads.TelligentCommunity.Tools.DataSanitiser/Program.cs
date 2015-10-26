19c066e51327e6e49229be009c03d9ad5d5fbe78
Rouslan Grabar <rouslan@gmail.com>
2015-10-26 16:46:40 +0400

    DataSanititser utility initial import

---
+++ src/code/FourRoads.TelligentCommunity.Tools.DataSanitiser/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+namespace FourRoads.TelligentCommunity.Tools.DataSanitiser
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
+            var log = LogManager.GetLogger("Sanitiser");
+
+            try
+            {
+                Sainitizer.Instance.Execute(log);
+            }
+            catch (Exception exception)
+            {
+                log.Fatal(exception.Message, exception);
+            }
+
+            Console.Write("Press any key...");
+            Console.ReadLine();
+        }
+    }
+}
