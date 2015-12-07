5e61bdc75feb8a5aa8b000ba14f7461c91778ea4
Rob Nash <robert.nash@4-roads.com>
2015-12-07 10:49:34 +0000

    missing files

---
+++ src/code/FourRoads.TelligentCommunity.ConfigurationExtensions/Resources/Widgets/4a653667e34e47b6b13bfa6e7eb67398/subscriptiondefaults.js
@@ -0,0 +1,58 @@
+﻿(function ($) {
+    if (typeof $.fourroads == 'undefined') {
+        $.fourroads = {};
+    }
+    if (typeof $.fourroads.widgets == 'undefined') {
+        $.fourroads.widgets = {};
+    }
+
+    var scrapeElements = function (context) {
+        $.each([context.selectors], function (i, set) {
+            $.each(set, function (key, value) {
+                set[key] = $(value);
+            });
+        });
+    };
+
+    $.fourroads.widgets.subscriptionDefaults =
+	{
+	    register: function (context) {
+	        scrapeElements(context);
+
+	        context.selectors.defaultSettingsRadio.change(function (e) {
+	            e.preventDefault();
+	            var setting = this.value;
+	            var calendarId = context.calendarId;
+
+	            $.telligent.evolution.post({
+	                url: context.urls.updateSettings,
+	                data: {
+	                    setting: setting,
+	                    calendarId: calendarId
+	                },
+	                success: function (response) {
+	                    $.telligent.evolution.notifications.show(context.resources.updatedMessage, { type: 'success' });
+	                }
+	            });
+	        });
+
+	        context.selectors.resetSiteButton.click(function (e) {
+	            e.preventDefault();
+	            var calendarId = context.calendarId;
+	            var groupId = context.groupId;
+
+	            $.telligent.evolution.post({
+	                url: context.urls.resetSettings,
+	                data: {
+	                    calendarId: calendarId,
+	                    groupId: groupId
+	                },
+	                success: function (response) {
+	                    $.telligent.evolution.notifications.show(context.resources.updatedMessage, { type: 'success' });
+	                }
+	            });
+	        });
+	    }
+	}
+
+})(jQuery);
