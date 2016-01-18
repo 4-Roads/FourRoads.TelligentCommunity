ac825cc33ab0ba895e9fb32f3627d814b78a2634
James Johnston <james.johnston@4-roads.com>
2016-01-18 09:26:01 +0000

    Added group digest default settings

---
+++ src/code/FourRoads.TelligentCommunity.ConfigurationExtensions/Resources/Widgets/52fcdd61ca2f4783a331628681290210/subscriptiondefaults.js
@@ -0,0 +1,59 @@
+﻿(function ($) {
+    if (typeof $.fourroads == 'undefined') {
+        $.fourroads = {};
+    }
+    if (typeof $.fourroads.widgets == 'undefined') {
+        $.fourroads.widgets = {};
+    }
+    if (typeof $.fourroads.widgets.group == 'undefined') {
+        $.fourroads.widgets.group = {};
+    }
+
+    var scrapeElements = function (context) {
+        $.each([context.selectors], function (i, set) {
+            $.each(set, function (key, value) {
+                set[key] = $(value);
+            });
+        });
+    };
+
+    $.fourroads.widgets.group.subscriptionDefaults =
+	{
+	    register: function (context) {
+	        scrapeElements(context);
+
+	        context.selectors.defaultSettingsRadio.change(function (e) {
+	            e.preventDefault();
+	            var setting = this.value;
+	            var groupId = context.groupId;
+
+	            $.telligent.evolution.post({
+	                url: context.urls.updateSettings,
+	                data: {
+	                    setting: setting,
+	                    groupId: groupId
+	                },
+	                success: function (response) {
+	                    $.telligent.evolution.notifications.show(context.resources.updatedMessage, { type: 'success' });
+	                }
+	            });
+	        });
+
+	        context.selectors.resetSiteButton.click(function (e) {
+	            e.preventDefault();
+	            var groupId = context.groupId;
+
+	            $.telligent.evolution.post({
+	                url: context.urls.resetSettings,
+	                data: {
+	                    groupId: groupId
+	                },
+	                success: function (response) {
+	                    $.telligent.evolution.notifications.show(context.resources.updatedMessage, { type: 'success' });
+	                }
+	            });
+	        });
+	    }
+	}
+
+})(jQuery);
---
+++ src/code/FourRoads.TelligentCommunity.ConfigurationExtensions/SubscriptionProcessors/GroupDigestUpdateProcessor.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq;
+using FourRoads.Common.TelligentCommunity.Components.Extensions;
+using FourRoads.TelligentCommunity.ConfigurationExtensions.Enumerations;
+using Telligent.Evolution.Extensibility.Api.Entities.Version1;
+using Telligent.Evolution.Extensibility.Api.Version1;
+using Telligent.Evolution.Extensibility.Content.Version1;
+using Telligent.Evolution.Extensibility.Jobs.Version1;
+
+namespace FourRoads.TelligentCommunity.ConfigurationExtensions.SubscriptionProcessors
+{
+
+    public class GroupDigestUpdateProcessor : SubscriptionUpdateProcessor
+    {
+
+        public override bool CanProcess(JobData jobData)
+        {
+            return jobData.Data.ContainsKey("processGroups");
+        }
+
+        protected override void InternalProcess(User user, Group @group, JobData jobData)
+        {
+            GroupEnumerate groupEnumerate = new GroupEnumerate(@group.Id.Value);
+
+            foreach (Group g in groupEnumerate)
+            {
+                var lookups = g.ExtendedAttributes.ToLookup(attribute => attribute.Key, val => (IExtendedAttribute)val);
+
+                string setting = lookups.GetString("DefaultDigestSetting", "unset");
+
+                if ("daily".Equals(setting))
+                {
+                    PublicApi.Users.RunAsUser(user.Id.Value, () =>
+                    {
+                        EmailDigestSubscription subscription = PublicApi.EmailDigestSubscriptions.GetByGroup(@group.Id.Value);
+                        if (subscription != null)
+                        {
+                            PublicApi.EmailDigestSubscriptions.Update(subscription.Id.Value, 1);
+                        }
+                        else
+                        {
+                            PublicApi.EmailDigestSubscriptions.Create("group", @group.Id.Value, 1);
+                        }
+                    });
+                }
+                else if ("weekly".Equals(setting))
+                {
+                    PublicApi.Users.RunAsUser(user.Id.Value, () =>
+                    {
+                        EmailDigestSubscription subscription = PublicApi.EmailDigestSubscriptions.GetByGroup(@group.Id.Value);
+                        if (subscription != null)
+                        {
+                            PublicApi.EmailDigestSubscriptions.Update(subscription.Id.Value, 7);
+                        }
+                        else
+                        {
+                            PublicApi.EmailDigestSubscriptions.Create("group", @group.Id.Value, 7);
+                        }
+                    });
+                }
+                else if("off".Equals(setting))
+                {
+                    PublicApi.Users.RunAsUser(user.Id.Value, () =>
+                    {
+                        EmailDigestSubscription subscription = PublicApi.EmailDigestSubscriptions.GetByGroup(@group.Id.Value);
+                        if (subscription != null)
+                        {
+                            PublicApi.EmailDigestSubscriptions.Delete(subscription.Id.Value);
+                        }
+                    });
+                }
+            }
+        }
+
+        public override string ProcessorName
+        {
+            get { return "Group"; }
+        }
+
+    }
+
+}
