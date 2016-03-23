bcd13114c09579a67b1feecc949edbfab5af88d1
Rhys Godfrey <rhys@ardourdigital.co.uk>
2016-03-23 14:42:28 +0000

    Adding Meta Data Image Rest API
    Adding an endpoint to allow the Meta Data image to be retrieved based on
    the content id and content type id.
    
    Returns the URL of the best image found in the content.

---
+++ src/code/FourRoads.TelligentCommunity.MetaData/Rest/Entities/ShowMetaDataResponse.cs
@@ -0,0 +1,13 @@
+﻿using Telligent.Evolution.Rest.Infrastructure.Version2;
+
+namespace FourRoads.TelligentCommunity.MetaData.Rest.Entities
+{
+    public class ShowMetaDataImageResponse : DefaultRestResponse
+    {
+        public ShowMetaDataImageResponse()
+        {
+            Name = "meta-data-image";
+            Errors = new string[0];
+        }
+    }
+}
