e69eb110230cf2f81585a78644eb9606543a8ff1
Karl Barber <karl.barber@4-roads.com>
2017-05-04 17:05:36 +0100

    Updated assembly info

--- src/code/FourRoads.TelligentCommunity.ForumThreadViews/Properties/AssemblyInfo.cs
+++ src/code/FourRoads.TelligentCommunity.ForumThreadViews/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -7,12 +6,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("FourRoads.TelligentCommunity100.ForumThreadViews")]
 [assembly: AssemblyDescription("Used to monitor when a user views a thread to provide a trigger for achievements")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FourRoads.TelligentCommunity.ForumThreadViews")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,15 +15,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1780110f-ef88-4134-8c8f-94e5c411b29d")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
