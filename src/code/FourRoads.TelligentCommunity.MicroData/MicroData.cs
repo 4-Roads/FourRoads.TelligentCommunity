548f69d7393a500422034992c2bf8747e21e6d3b
RobN <robert.nash@4-roads.com>
2018-03-14 15:47:00 +0000

    fixed MicroData initi as API is not available

--- src/code/FourRoads.TelligentCommunity.MicroData/MicroData.cs
+++ src/code/FourRoads.TelligentCommunity.MicroData/MicroData.cs
@@ -1,3 +1,4 @@
+using System;
 using Telligent.Evolution.Extensibility;
 using Telligent.Evolution.Extensibility.Api.Version1;
 
@@ -9,22 +10,22 @@ namespace FourRoads.TelligentCommunity.MicroData
         public static MicroDataEntry[] Entries = new[]
                 {
                     //Bogs
-                    new MicroDataEntry(){ContentType = Apis.Get<IBlogPosts>().ContentTypeId, Selector = ".layout" , Type = MicroDataType.itemscope , Value = "http://schema.org/BlogPosting"},
-                    new MicroDataEntry(){ContentType =Apis.Get<IBlogPosts>().ContentTypeId, Selector = ".blog-details .content-author" , Type = MicroDataType.itemprop , Value = "creator"},
-                    new MicroDataEntry(){ContentType =Apis.Get<IBlogPosts>().ContentTypeId, Selector = ".blog-details .content-date" , Type = MicroDataType.itemprop , Value = "datePublished"},
-                    new MicroDataEntry(){ContentType =Apis.Get<IBlogPosts>().ContentTypeId, Selector = ".blog-post .full .name" , Type = MicroDataType.itemprop , Value = "name"},
-                    new MicroDataEntry(){ContentType =Apis.Get<IBlogPosts>().ContentTypeId, Selector = ".blog-post .full .content" , Type = MicroDataType.itemprop , Value = "text"},
+                    new MicroDataEntry(){ContentType = new Guid("ca0e7c80-8686-4d2f-a5a8-63b9e212e922"), Selector = ".layout" , Type = MicroDataType.itemscope , Value = "http://schema.org/BlogPosting"},
+                    new MicroDataEntry(){ContentType =new Guid("ca0e7c80-8686-4d2f-a5a8-63b9e212e922"), Selector = ".blog-details .content-author" , Type = MicroDataType.itemprop , Value = "creator"},
+                    new MicroDataEntry(){ContentType =new Guid("ca0e7c80-8686-4d2f-a5a8-63b9e212e922"), Selector = ".blog-details .content-date" , Type = MicroDataType.itemprop , Value = "datePublished"},
+                    new MicroDataEntry(){ContentType =new Guid("ca0e7c80-8686-4d2f-a5a8-63b9e212e922"), Selector = ".blog-post .full .name" , Type = MicroDataType.itemprop , Value = "name"},
+                    new MicroDataEntry(){ContentType =new Guid("ca0e7c80-8686-4d2f-a5a8-63b9e212e922"), Selector = ".blog-post .full .content" , Type = MicroDataType.itemprop , Value = "text"},
                    
                     //Comments
-                    new MicroDataEntry(){ContentType =Apis.Get<IBlogPosts>().ContentTypeId, Selector = ".content-fragment.comments" , Type = MicroDataType.itemscope , Value = "http://schema.org/Comment"},
-                    new MicroDataEntry(){ContentType =Apis.Get<IBlogPosts>().ContentTypeId, Selector = ".content-fragment.comments .content.comment .content" , Type = MicroDataType.itemprop , Value = "text"},
-                    new MicroDataEntry(){ContentType =Apis.Get<IBlogPosts>().ContentTypeId, Selector = ".content-fragment.comments .content.comment .author" , Type = MicroDataType.itemprop , Value = "creator"},
-                    new MicroDataEntry(){ContentType = Apis.Get<IMedia>().ContentTypeId, Selector = ".content-fragment.comments" , Type = MicroDataType.itemscope , Value = "http://schema.org/Comment"},
-                    new MicroDataEntry(){ContentType = Apis.Get<IMedia>().ContentTypeId, Selector = ".content-fragment.comments .content.comment .content" , Type = MicroDataType.itemprop , Value = "text"},
-                    new MicroDataEntry(){ContentType = Apis.Get<IMedia>().ContentTypeId, Selector = ".content-fragment.comments .content.comment .author" , Type = MicroDataType.itemprop , Value = "creator"},
-                    new MicroDataEntry(){ContentType = Apis.Get<IWikiPages>().ContentTypeId, Selector = ".content-fragment.comments" , Type = MicroDataType.itemscope , Value = "http://schema.org/Comment"},
-                    new MicroDataEntry(){ContentType = Apis.Get<IWikiPages>().ContentTypeId, Selector = ".content-fragment.comments .content.comment .content" , Type = MicroDataType.itemprop , Value = "text"},
-                    new MicroDataEntry(){ContentType = Apis.Get<IWikiPages>().ContentTypeId, Selector = ".content-fragment.comments .content.comment .author" , Type = MicroDataType.itemprop , Value = "creator"},
+                    new MicroDataEntry(){ContentType =new Guid("ca0e7c80-8686-4d2f-a5a8-63b9e212e922"), Selector = ".content-fragment.comments" , Type = MicroDataType.itemscope , Value = "http://schema.org/Comment"},
+                    new MicroDataEntry(){ContentType =new Guid("ca0e7c80-8686-4d2f-a5a8-63b9e212e922"), Selector = ".content-fragment.comments .content.comment .content" , Type = MicroDataType.itemprop , Value = "text"},
+                    new MicroDataEntry(){ContentType =new Guid("ca0e7c80-8686-4d2f-a5a8-63b9e212e922"), Selector = ".content-fragment.comments .content.comment .author" , Type = MicroDataType.itemprop , Value = "creator"},
+                    new MicroDataEntry(){ContentType = new Guid("08ca0da0-e017-4a80-9832-476c74d4f174"), Selector = ".content-fragment.comments" , Type = MicroDataType.itemscope , Value = "http://schema.org/Comment"},
+                    new MicroDataEntry(){ContentType = new Guid("08ca0da0-e017-4a80-9832-476c74d4f174"), Selector = ".content-fragment.comments .content.comment .content" , Type = MicroDataType.itemprop , Value = "text"},
+                    new MicroDataEntry(){ContentType = new Guid("08ca0da0-e017-4a80-9832-476c74d4f174"), Selector = ".content-fragment.comments .content.comment .author" , Type = MicroDataType.itemprop , Value = "creator"},
+                    new MicroDataEntry(){ContentType = new Guid("6b577b8c-0470-4e20-9d29-b6772bf67243"), Selector = ".content-fragment.comments" , Type = MicroDataType.itemscope , Value = "http://schema.org/Comment"},
+                    new MicroDataEntry(){ContentType = new Guid("6b577b8c-0470-4e20-9d29-b6772bf67243"), Selector = ".content-fragment.comments .content.comment .content" , Type = MicroDataType.itemprop , Value = "text"},
+                    new MicroDataEntry(){ContentType = new Guid("6b577b8c-0470-4e20-9d29-b6772bf67243"), Selector = ".content-fragment.comments .content.comment .author" , Type = MicroDataType.itemprop , Value = "creator"},
 
                     //Users
                     new MicroDataEntry(){ContentType = null, Selector = ".content-author, .author" , Type = MicroDataType.itemscope , Value = "http://schema.org/Person"},
@@ -33,19 +34,19 @@ namespace FourRoads.TelligentCommunity.MicroData
                     new MicroDataEntry(){ContentType = null, Selector = ".content-author .avatar, .author .avatar img" , Type = MicroDataType.itemprop , Value = "image"},
                     
                     //FOrums
-                    new MicroDataEntry(){ContentType = Apis.Get<IForumThreads>().ContentTypeId, Selector = ".layout" , Type = MicroDataType.itemscope , Value = "http://schema.org/Article"},
-                    new MicroDataEntry(){ContentType = Apis.Get<IForumThreads>().ContentTypeId, Selector = ".thread .full" , Type = MicroDataType.itemscope , Value = "http://schema.org/Comment"},
-                    new MicroDataEntry(){ContentType = Apis.Get<IForumThreads>().ContentTypeId, Selector = ".thread .author" , Type = MicroDataType.itemprop , Value = "creator"},
-                    new MicroDataEntry(){ContentType = Apis.Get<IForumThreads>().ContentTypeId, Selector = ".thread .full .name" , Type = MicroDataType.itemprop , Value = "name"},
-                    new MicroDataEntry(){ContentType = Apis.Get<IForumThreads>().ContentTypeId, Selector = ".thread .full .content" , Type = MicroDataType.itemprop , Value = "text"},
+                    new MicroDataEntry(){ContentType = new Guid("46448885-d0e6-4133-bbfb-f0cd7b0fd6f7"), Selector = ".layout" , Type = MicroDataType.itemscope , Value = "http://schema.org/Article"},
+                    new MicroDataEntry(){ContentType = new Guid("46448885-d0e6-4133-bbfb-f0cd7b0fd6f7"), Selector = ".thread .full" , Type = MicroDataType.itemscope , Value = "http://schema.org/Comment"},
+                    new MicroDataEntry(){ContentType = new Guid("46448885-d0e6-4133-bbfb-f0cd7b0fd6f7"), Selector = ".thread .author" , Type = MicroDataType.itemprop , Value = "creator"},
+                    new MicroDataEntry(){ContentType = new Guid("46448885-d0e6-4133-bbfb-f0cd7b0fd6f7"), Selector = ".thread .full .name" , Type = MicroDataType.itemprop , Value = "name"},
+                    new MicroDataEntry(){ContentType = new Guid("46448885-d0e6-4133-bbfb-f0cd7b0fd6f7"), Selector = ".thread .full .content" , Type = MicroDataType.itemprop , Value = "text"},
                     
                     
                     //Media
-                    new MicroDataEntry(){ContentType = Apis.Get<IMedia>().ContentTypeId, Selector = ".layout" , Type = MicroDataType.itemscope , Value = "http://schema.org/MediaObject"},
-                    new MicroDataEntry(){ContentType = Apis.Get<IMedia>().ContentTypeId, Selector = ".media-gallery-post .full .name a" , Type = MicroDataType.itemprop , Value = "contentUrl"},
-                    new MicroDataEntry(){ContentType = Apis.Get<IMedia>().ContentTypeId, Selector = ".media-gallery-post  .full .name" , Type = MicroDataType.itemprop , Value = "name"},
-                    new MicroDataEntry(){ContentType = Apis.Get<IMedia>().ContentTypeId, Selector = ".media-gallery-post .author" , Type = MicroDataType.itemprop , Value = "creator"},
-                    new MicroDataEntry(){ContentType = Apis.Get<IMedia>().ContentTypeId, Selector = ".media-gallery-post .full .content" , Type = MicroDataType.itemprop , Value = "text"},
+                    new MicroDataEntry(){ContentType = new Guid("08ca0da0-e017-4a80-9832-476c74d4f174"), Selector = ".layout" , Type = MicroDataType.itemscope , Value = "http://schema.org/MediaObject"},
+                    new MicroDataEntry(){ContentType = new Guid("08ca0da0-e017-4a80-9832-476c74d4f174"), Selector = ".media-gallery-post .full .name a" , Type = MicroDataType.itemprop , Value = "contentUrl"},
+                    new MicroDataEntry(){ContentType = new Guid("08ca0da0-e017-4a80-9832-476c74d4f174"), Selector = ".media-gallery-post  .full .name" , Type = MicroDataType.itemprop , Value = "name"},
+                    new MicroDataEntry(){ContentType = new Guid("08ca0da0-e017-4a80-9832-476c74d4f174"), Selector = ".media-gallery-post .author" , Type = MicroDataType.itemprop , Value = "creator"},
+                    new MicroDataEntry(){ContentType = new Guid("08ca0da0-e017-4a80-9832-476c74d4f174"), Selector = ".media-gallery-post .full .content" , Type = MicroDataType.itemprop , Value = "text"},
                 };
 
     }
