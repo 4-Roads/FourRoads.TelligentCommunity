ff0c149dbc208a741580d130d663faf30cbb9780
RobN <robert.nash@4-roads.com>
2019-09-30 22:46:16 +0100

    removed retired project

--- src/code/FourRoads.TelligentCommunity.Performance/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("FourRoads.TelligentCommunity101.Performance")]
-[assembly: AssemblyDescription("Improves page rendering speed by bundling and minifying CSS and Javascript assets")]
