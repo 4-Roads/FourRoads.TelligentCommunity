8ef05d1194432fae6b11b2e909a8457311192d82
RobN <robert.nash@4-roads.com>
2019-09-30 22:34:38 +0100

    removed custom editor project

--- src/code/FourRoads.TelligentCommunity.CustomEditor/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("FourRoads.TelligentCommunity101.CustomEditor")]
-[assembly: AssemblyProduct("FourRoads.TelligentCommunity.CustomEditor")]
-[assembly: AssemblyDescription("Enables the use of custom editors")]
-
--- src/code/FourRoads.TelligentCommunity.CustomEditor/readme.md
+++
@@ -1,5 +0,0 @@
-#Custom Editor
-
-##Introduction
-This plugin allows you to replace the existing TinyMCE editor with a custom version, the plugin is delivered with Redactor as default which requires a license.
-
