5b5a144c9c03f88f331c63bae217a89fdd8e649e
RobN <robert.nash@4-roads.com>
2020-01-17 16:37:55 +0000

    fixed last read post

--- src/code/FourRoads.TelligentCommunity.ForumLastPost/Logic/LastReadPost.cs
+++ src/code/FourRoads.TelligentCommunity.ForumLastPost/Logic/LastReadPost.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FourRoads.Common.TelligentCommunity.Plugins.Base;
 using FourRoads.TelligentCommunity.ForumLastPost.Interfaces;
 using Telligent.Common;
 using Telligent.Evolution.Components;
@@ -14,67 +15,58 @@ namespace FourRoads.TelligentCommunity.ForumLastPost.Logic
     {
         private ICacheService _cacheService;
         private ILastReadPostDataProvider _lastReadPostDataProvider;
+        private IUsers _users;
+        private IForumReplies _forumReplies;
+        private IForumThreads _forumThreads;
 
-        public LastReadPost(ICacheService cacheService , ILastReadPostDataProvider lastReadPostDataProvider )
+        public LastReadPost(ICacheService cacheService , ILastReadPostDataProvider lastReadPostDataProvider, IUsers users , IForumReplies forumReplies, IForumThreads forumThreads)
         {
             _lastReadPostDataProvider = lastReadPostDataProvider;
             _cacheService = cacheService;
+            _users = users;
+            _forumReplies = forumReplies;
+            _forumThreads = forumThreads;
         }
 
         public void UpdateLastReadPost(Guid appicationId, int userId, int threadId , int forumId, int replyId,  Guid lastReadContentId , DateTime postDateTime)
         {
-            //Not API safe but can't see a way to get the current context
-            //
-            var content = Services.Get<IWebContextService>().GetCurrentContent(Services.Get<IContextService>().GetHttpContext());
+            ForumThread thread = _forumThreads.Get(threadId, new ForumThreadsGetOptions() {ForumId = forumId});
 
-            if (content == null)
-                return;
+            LastReadPostInfo lastReadPost = GetLastReadPost(appicationId, thread.ContentId, userId);
 
-            if (Apis.Get<IContentTypes>().Get(content.ContentTypeId).Name.ToLower() != "forum thread")
-                return;
-
-            ForumReply post = Apis.Get<IForumReplies>().Get(replyId, new ForumRepliesGetOptions() { ForumId = forumId, ThreadId = threadId });
-
-            if (post != null && post.ThreadId == threadId)
+            if (lastReadPost.ContentId.GetValueOrDefault(Guid.Empty) != lastReadContentId)
             {
-                ForumThread thread = Apis.Get<IForumThreads>().Get(threadId, new ForumThreadsGetOptions() {ForumId = forumId});
-
-                LastReadPostInfo lastReadPost = GetLastReadPost(appicationId, thread.ContentId, userId);
-
-                if (lastReadPost.ContentId.GetValueOrDefault(Guid.Empty) != lastReadContentId)
+                //Get this posts post date
+                if (postDateTime > lastReadPost.PostDate)
                 {
-                    //Get this posts post date
-                    if (postDateTime > lastReadPost.PostDate)
+                    //Work out the reply count all posts previous to this date
+                    int pageIndex = _forumReplies.GetPageIndex(threadId, replyId, new ForumRepliesGetPageIndexOptions() {IncludeThreadStarter = true, PageSize = 10});
+                    int replyCount = pageIndex*10;
+
+                    var replies = _forumReplies.List(threadId, new ForumRepliesListOptions() {PageIndex = pageIndex, PageSize = 10, IncludeThreadStarter = true});
+
+                    foreach (ForumReply forumReply in replies)
                     {
-                        //Work out the reply count all posts previous to this date
-                        int pageIndex = Apis.Get<IForumReplies>().GetPageIndex(threadId, replyId, new ForumRepliesGetPageIndexOptions() {IncludeThreadStarter = true, PageSize = 10});
-                        int replyCount = pageIndex*10;
-
-                        var replies = Apis.Get<IForumReplies>().List(threadId, new ForumRepliesListOptions() {PageIndex = pageIndex, PageSize = 10, IncludeThreadStarter = true});
-
-                        foreach (ForumReply forumReply in replies)
+                        if (forumReply.Id != replyId)
                         {
-                            if (forumReply.Id != replyId)
-                            {
-                                replyCount++;
-                            }
-                            else
-                            {
-                                break;
-                            }
+                            replyCount++;
                         }
-
-                        _lastReadPostDataProvider.UpdateLastReadPost(appicationId, thread.ContentId, userId, lastReadContentId, replyCount, postDateTime);
-
-                        lastReadPost.PostDate = postDateTime;
-                        lastReadPost.ContentId = lastReadContentId;
-                        lastReadPost.ReplyCount = replyCount;
-
-                        string key = CreateCacheKey(appicationId, thread.ContentId, userId);
-
-                        _cacheService.Remove(key, CacheScope.Process);
-                        _cacheService.Put(key, lastReadPost, CacheScope.Process);
+                        else
+                        {
+                            break;
+                        }
                     }
+
+                    _lastReadPostDataProvider.UpdateLastReadPost(appicationId, thread.ContentId, userId, lastReadContentId, replyCount, postDateTime);
+
+                    lastReadPost.PostDate = postDateTime;
+                    lastReadPost.ContentId = lastReadContentId;
+                    lastReadPost.ReplyCount = replyCount;
+
+                    string key = CreateCacheKey(appicationId, thread.ContentId, userId);
+
+                    _cacheService.Remove(key, CacheScope.Process);
+                    _cacheService.Put(key, lastReadPost, CacheScope.Process);
                 }
             }
         }
@@ -93,6 +85,21 @@ namespace FourRoads.TelligentCommunity.ForumLastPost.Logic
             }
 
             return lastReadPost.Value;
+        }
+
+        public void SetLastReadPost(Guid appicationId, Guid contentId)
+        {
+            var user = _users.AccessingUser;
+
+            if (!user.IsSystemAccount.GetValueOrDefault(false) && user.Id.HasValue)
+            {
+                var forumReply = _forumReplies.Get(contentId);
+
+                forumReply.ThrowErrors();
+
+                UpdateLastReadPost(forumReply.Application.ApplicationId, user.Id.Value, forumReply.ThreadId.GetValueOrDefault(0),
+                    forumReply.ForumId.GetValueOrDefault(0), forumReply.Id.GetValueOrDefault(0), forumReply.ContentId, forumReply.Date.GetValueOrDefault(DateTime.MinValue));
+            }
         }
 
         private string CreateCacheKey(Guid appicationId, Guid contentId, int userId)
