0565caf9f85aba64574679d614b8c9f8109b65a6
RobN <robert.nash@4-roads.com>
2020-02-19 21:46:19 +0000

    fixed issue with content rendering helper where it is not intercepting a real HTML document and needs to just simply output the current stream

--- src/code/FourRoads.TelligentCommunity.RenderingHelper/RenderingHelperFilter.cs
+++ src/code/FourRoads.TelligentCommunity.RenderingHelper/RenderingHelperFilter.cs
@@ -67,12 +67,19 @@ namespace FourRoads.TelligentCommunity.RenderingHelper
                             IHtmlDocument document = parser.Parse(tmpStream);
                             //CQ document = CQ.CreateDocument(tmpStream, HttpContext.Current.Response.ContentEncoding);
 
-                            _renderingObserverPlugin.NotifyObservers(document);
-
-                            using (StreamWriter sw = new StreamWriter(_sink, HttpContext.Current.Response.ContentEncoding,(int) Length + 1000))
+                            if (document.Doctype != null)
                             {
-                                sw.Write(document.DocumentElement.OuterHtml);
-                                //document.Render(OutputFormatters.HtmlEncodingNone, sw);
+                                _renderingObserverPlugin.NotifyObservers(document);
+
+                                using (StreamWriter sw = new StreamWriter(_sink, HttpContext.Current.Response.ContentEncoding, (int)Length + 1000))
+                                {
+                                    sw.Write(document.DocumentElement.OuterHtml);
+                                }
+                            }
+                            else
+                            {
+                                Seek(0, SeekOrigin.Begin);
+                                CopyTo(_sink);
                             }
                         }
                     }
@@ -100,7 +107,7 @@ namespace FourRoads.TelligentCommunity.RenderingHelper
 
             if (!resposne.IsRequestBeingRedirected && (resposne.StatusCode > 199 && resposne.StatusCode < 300))
             {
-            _isClosing = true;
+                _isClosing = true;
             }
 
             Flush();
