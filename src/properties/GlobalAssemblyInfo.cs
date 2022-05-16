b370c56c6eca344b36e569c88a427db9fe032302
Rouslan Grabar <rouslan@gmail.com>
2022-05-16 12:36:17 +0400

    Bumped major version to 21

--- src/properties/GlobalAssemblyInfo.cs
+++ src/properties/GlobalAssemblyInfo.cs
@@ -19,6 +19,6 @@ using System.Reflection;
 #else
 [assembly: AssemblyConfiguration("Release")]
 #endif
-[assembly: AssemblyVersion("20.0.0.0")]
-[assembly: AssemblyFileVersion("20.0.0.0")]
-[assembly: AssemblyInformationalVersion("20.0.0.0")]
+[assembly: AssemblyVersion("21.0.0.0")]
+[assembly: AssemblyFileVersion("21.0.0.0")]
+[assembly: AssemblyInformationalVersion("21.0.0.0")]
