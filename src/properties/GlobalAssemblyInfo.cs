ca1f67a43e1cb59948b86733363310b69b9a6309
RobN <robert.nash@4-roads.com>
2023-06-20 14:33:42 +0100

    Updated copywrite

--- src/properties/GlobalAssemblyInfo.cs
+++ src/properties/GlobalAssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Reflection;
 #endregion
 
 [assembly: AssemblyCompany("4 Roads Ltd")]
-[assembly: AssemblyCopyright("Copyright © 4 Roads Ltd 2012-2018")]
+[assembly: AssemblyCopyright("Copyright © 4 Roads Ltd 2012-2023")]
 [assembly: AssemblyTrademark("All rights reserved.")]
 [assembly: AssemblyCulture("")]
 #if DEBUG
